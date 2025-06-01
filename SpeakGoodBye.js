a500542a11a4d9eb8ae996b2a33893fc347eabf0
okarsamer <111206470+okarsamer@users.noreply.github.com>
2025-06-01 22:20:59 +0200

    Create SpeakGoodBye.js

---
+++ SpeakGoodBye.js
@@ -0,0 +1,16 @@
+// STEP 5: Wrap in IIFE
+(function (window) {
+
+  // STEP 6: Create byeSpeaker object
+  var byeSpeaker = {};
+
+  // STEP 7: Attach speak method to byeSpeaker
+  var speakWord = "Good Bye";
+  byeSpeaker.speak = function (name) {
+    console.log(speakWord + " " + name);
+  };
+
+  // STEP 8: Expose byeSpeaker to the global scope
+  window.byeSpeaker = byeSpeaker;
+
+})(window);
