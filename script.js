1e54a0919ca3aad2c03962feb812b7419ba3f202
okarsamer <111206470+okarsamer@users.noreply.github.com>
2025-06-01 22:20:15 +0200

    Create script.js

---
+++ script.js
@@ -0,0 +1,18 @@
+// STEP 9: Wrap in IIFE
+(function () {
+
+  var names = ["Yaakov", "John", "Jen", "Jason", "Paul", "Frank", "Larry", "Paula", "Laura", "Jim"];
+
+  // STEP 10: Loop through names
+  for (var i = 0; i < names.length; i++) {
+    var firstLetter = names[i].charAt(0).toLowerCase();
+
+    // STEP 11: Compare first letter and call appropriate speak method
+    if (firstLetter === 'j') {
+      byeSpeaker.speak(names[i]);
+    } else {
+      helloSpeaker.speak(names[i]);
+    }
+  }
+
+})();
